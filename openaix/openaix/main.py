8b8aab5d549fd653545d912293166cf071a3a913
mrkvn <52745562+mrkvn@users.noreply.github.com>
2023-11-16 00:59:29 +0800

    connect to API. simple chat

--- openaix/openaix/main.py
+++ openaix/openaix/main.py
@@ -1,5 +1,29 @@
+import os
+
+import dotenv
+from openai import OpenAI
+
+dotenv.load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
 def main():
-    print("Hello World!")
+    client = OpenAI(api_key=OPENAI_API_KEY)
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are very concise. \
+                    You only do what you are told to do and you answer as short as possible.",
+            },
+            {"role": "user", "content": "Just reply TEST"},
+        ],
+    )
+
+    print(completion.choices[0].message)
 
 
 if __name__ == "__main__":
